e977e7f0e70b8602712f9ce5e55acc52a7c87e2b
binh <23639111.binh@student.iuh.edu.vn>
2024-08-27 16:47:30 +0700

    message

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"edu.iuh.fit.se","l":"Course"},{"p":"edu.iuh.fit.se","l":"CourseList"},{"p":"<Unnamed>","l":"Main"},{"p":"edu.iuh.fit.se","l":"TestCourse"}];updateSearchResults();
